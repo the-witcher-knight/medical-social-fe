53fc62e8bc301c65e353438f989e88666dd7b4e2
virsavik <dhuuloc8818@gmail.com>
2022-03-25 23:03:41 +0700

    fix format date

--- src/shared/util/time-ultil.js
+++ src/shared/util/time-ultil.js
@@ -1,3 +1,5 @@
+import dayjs from 'dayjs';
+
 /**
  * Combine the time and date into a single string
  * @param {Date} date only the date part
@@ -5,7 +7,7 @@
  * @returns {string} shape of "YYYY-MM-DDTHH:mm:ssZ"
  */
 export const combineDateAndTime = (date, time) => {
-  const datePart = date.format('YYYY-MM-DD');
-  const timePart = time.format('HH:mm:ss');
+  const datePart = dayjs(date).format('YYYY-MM-DD');
+  const timePart = dayjs(time).format('HH:mm:ss');
   return `${datePart}T${timePart}Z`;
 };
