d47d9c4001c914ea162e3f4666635d9a9e4c1e6b
virsavik <dhuuloc8818@gmail.com>
2022-03-29 20:06:45 +0700

    make welcome page

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,19 +1,16 @@
 import { Box, Typography } from '@mui/material';
-import React, { useEffect } from 'react';
-import { useAppDispatch, useAppSelector } from 'src/configs/store';
-import { getAccount } from 'src/shared/reducers/authentication';
+import React from 'react';
+import { parseJwt } from 'src/shared/util/auth-util';
+import { StorageAPI } from 'src/shared/util/storage-util';
 
 const WelcomePage = () => {
-  const dispatch = useAppDispatch();
-  const account = useAppSelector(state => state.authentication.account);
-
-  useEffect(() => {
-    dispatch(getAccount());
-  }, []);
+  const account = parseJwt(
+    StorageAPI.local.get('authToken') || StorageAPI.session.get('authToken')
+  );
 
   return (
     <Box component="div" mt={5} sx={{ display: 'flex', justifyContent: 'center' }}>
-      <Typography variant="h4">Welcome to the app, {account.login}</Typography>
+      <Typography variant="h4">Welcome to the app, {account.sub}</Typography>
     </Box>
   );
 };
