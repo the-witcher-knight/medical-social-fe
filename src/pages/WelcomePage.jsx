5b14368080260233eb71c779948a3e8bfbb8234f
virsavik <dhuuloc8818@gmail.com>
2022-05-04 01:47:13 +0700

    Update welcome page

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,34 +1,66 @@
-import { Box, Button, Paper, Typography, Link as MuiLink } from '@mui/material';
+import { Box, Button, Paper, Typography, Link as MuiLink, styled } from '@mui/material';
 import React from 'react';
-import { parseJwt } from 'src/shared/util/auth-util';
-import { StorageAPI } from 'src/shared/util/storage-util';
+import { getUserAuthentication } from 'src/shared/util/auth-util';
 import { Link } from 'react-router-dom';
+import { blue } from '@mui/material/colors';
+
+const StyledImage = styled('img')(({ theme }) => ({
+  maxWidth: '350px',
+  height: 'auto',
+  marginRight: theme.spacing(2),
+}));
+
+const StyledTitle = styled(Typography)(({ theme }) => ({
+  [theme.breakpoints.up('xs')]: {
+    fontSize: '2.5rem',
+  },
+  [theme.breakpoints.up('md')]: {
+    fontSize: '4rem',
+  },
+  fontWeight: 'bold',
+  marginBottom: theme.spacing(2),
+  color: blue[600],
+}));
+
+const StyledGreeting = styled(Typography)(({ theme }) => ({
+  [theme.breakpoints.up('xs')]: {
+    fontSize: '1.5rem',
+  },
+  [theme.breakpoints.up('md')]: {
+    fontSize: '2rem',
+  },
+  fontWeight: 'light',
+  marginBottom: theme.spacing(2),
+}));
 
 const WelcomePage = () => {
-  const account = parseJwt(
-    StorageAPI.local.get('authToken') || StorageAPI.session.get('authToken')
-  );
+  const account = getUserAuthentication();
 
   return (
-    <Box component="div" mt={5} display="flex" flexDirection="column" justifyContent="center">
-      <Paper elevation={3}>
-        <Box component="div" p={2} height="60vh">
-          <Typography variant="h5" align="center" component="h1">
-            Welcome to Medical Social Application
-          </Typography>
-          {account && account.sub ? (
-            <Typography variant="body1">Greeting {account.sub}</Typography>
-          ) : (
-            <Typography>
-              You seem to be not logged in, please &nbsp;
-              <MuiLink component={Link} to="/authorization/sign-in" variant="body2">
-                login
-              </MuiLink>
-            </Typography>
-          )}
-        </Box>
-      </Paper>
-    </Box>
+    <Paper
+      elevation={2}
+      sx={{ margin: 5, display: 'flex', flexDirection: { xs: 'column', md: 'row' }, padding: 3 }}
+    >
+      <StyledImage src="/medical-robot.jpg" alt="Medical Robot" />
+      <Box sx={{ flex: 1, padding: 3 }}>
+        <StyledTitle>Welcome to the Medical Social</StyledTitle>
+        {account && account.sub ? (
+          <StyledGreeting>Greeting {account.sub}</StyledGreeting>
+        ) : (
+          <StyledGreeting>
+            Please &nbsp;
+            <MuiLink component={Link} to="/authorization/sign-in">
+              SignIn
+            </MuiLink>
+            &nbsp; or &nbsp;
+            <MuiLink component={Link} to="/authorization/sign-up">
+              SignUp
+            </MuiLink>
+            &nbsp; to continue.
+          </StyledGreeting>
+        )}
+      </Box>
+    </Paper>
   );
 };
 export default WelcomePage;
